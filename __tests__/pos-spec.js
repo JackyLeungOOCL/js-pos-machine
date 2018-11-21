dd1837221d864c109757ae543b8407f8efaa25d6
jacky.leung <jacky.leung@oocl.com>
2018-11-21 17:25:11 +0800

    [LEUNGJA] feat: get item detail

--- __tests__/pos-spec.js
+++ __tests__/pos-spec.js
@@ -1,1 +1,34 @@
-'use strict'
+'use strict';
+const pos = require('../pos');
+
+it("getItemDetail should return item detail", function() {
+    // Given
+    const barcode = "ITEM000001";
+    const inventory = [
+        {
+            barcode: 'ITEM000000',
+            name: '可口可乐',
+            unit: '瓶',
+            price: 3.00
+        },
+        {
+            barcode: 'ITEM000001',
+            name: '雪碧',
+            unit: '瓶',
+            price: 3.00
+        }
+    ]
+
+    // When
+    const itemDetail = pos.getItemDetail(barcode, inventory);
+    const expected = 
+        {
+            barcode: 'ITEM000001',
+            name: '雪碧',
+            unit: '瓶',
+            price: 3.00
+        };
+
+    // Then
+    expect(itemDetail).toEqual(expected);
+});
